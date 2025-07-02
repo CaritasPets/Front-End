786c7da95d1fca3cc77424fa8e0e0fc5a922c0c8
AndreRicardoSc <andrenarutto7@gmail.com>
2025-07-02 16:49:59 -0300

    Created initial files and folders

---
+++ src/stores/store.js
@@ -0,0 +1,8 @@
+import { defineStore } from "pinia";
+
+export const store = defineStore('nome-store', () => {
+    //criação do conteudo da store
+    return {
+        //conteudos criados
+    }
+})
