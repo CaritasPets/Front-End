a59eef1f37790c98414242ef44fdc1b24b0542f7
Johann5731 <johannmborges27@gmail.com>
2025-07-16 16:35:52 -0300

    FEAT: Creating the header animations #1

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,33 +1,36 @@
 <script setup>
 import { ref } from 'vue'
+import { RouterLink } from 'vue-router';
 
 const hoveredIndex = ref(null)
 
 const navItems = [
-  { label: 'Home', icon: 'mdi mdi-home', link: '#' },
-  { label: 'Ongs', icon: 'mdi mdi-hand-heart', link: '#' },
-  { label: 'Adote', icon: 'mdi mdi-heart', link: '#' },
-  { label: 'Procura-se', icon: 'mdi mdi-magnify', link: '#' },
-  { label: 'Sobre', icon: 'mdi mdi-information', link: '#' },
+  { label: 'Home', icon: 'mdi mdi-home', link: '/' },
+  { label: 'Ongs', icon: 'mdi mdi-hand-heart', link: '/' },
+  { label: 'Adote', icon: 'mdi mdi-heart', link: '/' },
+  { label: 'Procura-se', icon: 'mdi mdi-magnify', link: '/' },
+  { label: 'Sobre', icon: 'mdi mdi-information', link: '/' },
 ]
 </script>
 <template>
-  <img src="/home/johann.borges/Programação/Front-End/public/logo.png" alt="Logo">
+  <RouterLink to="/">
+    <img src="/logo.png" alt="Logo">
+  </RouterLink>
   <nav>
     <ul>
-       <li v-for="(item, index) in navItems" :key="index">
-        <a :href="item.link">
+       <li v-for="(item, index) in navItems" :key="index" @mouseover="hoveredIndex = index" @mouseout="hoveredIndex = null">
+        <RouterLink :to="item.link">
           <span :class="item.icon" v-if="hoveredIndex == index"></span>
-          <h2 @mouseover="hoveredIndex = index" @mouseout="hoveredIndex = null">
+          <h2>
             {{ item.label }}
           </h2>
-        </a>
+        </RouterLink>
        </li>
     </ul>
   </nav>
-  <a href="">
+  <RouterLink to="">
     <span class="mdi mdi-account-circle-outline"></span>
-  </a>
+  </RouterLink>
 </template>
 <style scoped>
 nav ul {
@@ -43,18 +46,32 @@ nav ul li {
 nav ul li a {
   text-decoration: none;
   color: #1E0B00;
-  font-size: 1.8rem;
+  font-size: 1.5rem;
   transition: 0.5s;
   display: flex;
   align-items: center;
 
   span{
-    font-size: 3.5rem;
+    font-size: 3rem;
   }
 }
-
+span.mdi-account-circle-outline{
+  position: relative;
+}
 span {
   color: #1E0B00;
   font-size: 4.5rem;
+  animation: spanAnimation ease-in 0.5s;
+  position: absolute;
+  transform: translateX(-100%);
+}
+@keyframes spanAnimation{
+  0%{
+    transform: translateX(0);
+    opacity: 0;
+  } 100% {
+    transform: translateX(-100%);
+    opacity: 1;
+  }
 }
 </style>
