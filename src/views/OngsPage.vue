c4f007c7f3593a19e26091f02f0898a7b26049f5
AndreRicardoSc <andrenarutto7@gmail.com>
2025-07-18 13:54:23 -0300

    FEAT: Adding header and banner in ongs page #2

--- src/views/OngsPage.vue
+++ src/views/OngsPage.vue
@@ -1,13 +1,30 @@
 <script setup>
-    import PapelOngs from '../components/PapelOngs.vue';
+import BannerComponent from '../components/BannerComponent.vue';
+import HeaderComponent from '../components/HeaderComponent.vue'
+import PapelOngs from '../components/PapelOngs.vue'
+import { useBannerComponentStore } from '../stores/BannerComponentStore';
+const bannerStore = useBannerComponentStore();
 </script>
 <template>
-    <section class="papel-ongs">
-        <PapelOngs/>
-    </section>
+  <header>
+    <HeaderComponent/>
+  </header>
+  <section class="banner">
+    <BannerComponent
+      :background="bannerStore.propriedadesBanners.banner2.background"
+      :waves="bannerStore.propriedadesBanners.banner2.waves"
+      :title="bannerStore.propriedadesBanners.banner2.title"
+      :text="bannerStore.propriedadesBanners.banner2.text"
+      :image="bannerStore.propriedadesBanners.banner2.image"
+    />
+  </section>
+  <section class="papel-ongs">
+    <PapelOngs />
+  </section>
 </template>
 <style scoped>
-    section.papel-ongs{
-        margin: 0 10%;
-    }
+section.papel-ongs {
+  margin: 10% 10%;
+  margin-top: 45%;
+}
 </style>
