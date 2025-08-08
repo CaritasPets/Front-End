d8060492e3567897caa39aa1f3f01e09b2fab96e
AndreRicardoSc <andrenarutto7@gmail.com>
2025-08-07 22:30:41 -0300

    FIX: Change the CardFuncionalidade component for use Tailwindcss #test

--- src/stores/CardFuncionalidadeStore.js
+++ src/stores/CardFuncionalidadeStore.js
@@ -4,14 +4,14 @@ import { ref } from "vue";
 export const useCardFuncionalidadeStore = defineStore('card-store', () => {
     const propriedades = ref({
         card1: {
-            text: 'De um lar para um pet',
+            text: 'Dê um lar para um pet',
             icon: 'mdi mdi-home-heart',
             background: '#03497B',
             iconBackground: '#69A2A8',
             link: '/'
         },
         card2: {
-            text: 'Divulgue pets para adção',
+            text: 'Divulgue pets para adoção',
             icon: 'mdi mdi-bullhorn',
             background: '#587911',
             iconBackground: '#104C00',
