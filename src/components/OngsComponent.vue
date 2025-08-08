2b616d60f7d24f7a8baa815ed4af716b7754cee0
AndreRicardoSc <andrenarutto7@gmail.com>
2025-08-08 16:25:00 -0300

    FIX: Change the Ongs and PapelOngs components to use the tailwindcss #test

--- src/components/OngsComponent.vue
+++ src/components/OngsComponent.vue
@@ -1,50 +1,14 @@
 <script setup>
-    import { onMounted } from 'vue';
-
     const props = defineProps(['logo', 'nome', 'background']);
     const backgrounds = ['#03497B', '#587911', '#FDA202'];
     function sortBackground(){
         const randomNum = Math.floor(Math.random() * 3);
         return backgrounds[randomNum];
     }
-    
-    const myObserver =  new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if(entry.isIntersecting){
-                entry.target.classList.add("show")
-            }
-        })
-    })
-
-    onMounted(() => {
-        const cardOngElement = document.querySelector('.card-ong');
-        if(cardOngElement.value){
-            myObserver.observe(cardOngElement.value)
-        }
-    })
 </script>
 <template>    
-    <div class="card-ong" :style="`background: ${sortBackground()}`">
-        <img :src="props.logo" :alt="`${props.nome}-logo`">
-        <p class="nome">{{ props.nome }}</p>
+    <div class="flex flex-col items-center p-10 w-[calc(100%/5)] my-2 mx-1 text-center rounded-4xl cursor-pointer transition-all duration-500 hover:opacity-60" :style="`background: ${sortBackground()}`">
+        <img class="rounded-full w-[100%]" :src="props.logo" :alt="`${props.nome}-logo`">
+        <p class="text-white text-2xl">{{ props.nome }}</p>
     </div>
 </template>
-<style scoped>
-    div.card-ong {
-        padding: 3%;
-        width: calc(100%/5);
-        margin: 2% 1%;
-        text-align: center;
-        border-radius: 8%;
-        transition: all 0.5s;
-
-        img{
-           width: 80%;
-           border-radius: 80px; 
-        }
-        p{
-            color: #fff;
-            font-size: 200%;
-        }
-    }
-</style>
--- src/components/PapelOngs.vue
+++ src/components/PapelOngs.vue
@@ -1,54 +1,24 @@
-<script setup>
-import { onMounted } from 'vue'
-const myObserver = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    const elementClass = entry.target.classList
-    if (entry.isIntersecting) {
-      if (elementClass == 'hidden') {
-        entry.target.classList.add('show')
-      }
-    }
-  })
-})
-onMounted(() => {
-  const elements = document.querySelectorAll('.hidden')
-  elements.forEach((element) => myObserver.observe(element))
-})
-</script>
 <template>
-  <h2>O papel das ONGs</h2>
-  <p class="hidden">
-    As ONGs desempenham um papel crucial na defesa dos direitos dos animais, atuando no resgate,
-    acolhimento, tratamento e adoção responsável de animais abandonados ou vítimas de maus-tratos
-  </p>
-  <img class="hidden" src="/imagem_ongs.svg" alt="" />
-  <p class="hidden">
-    Voluntários dedicam seu tempo para transformar a angústia em acolhimento. Com cuidado, carinho e compromisso, eles reconquistam a confiança dos animais, oferecendo conforto, amor, saúde e a chance de um novo lar. Após esse processo de recuperação, as ONGs disponibilizam os pets para adoção, esperando que toquem os corações de quem busca um verdadeiro  companheiro.
-  </p>
+  <h2 class="text-center text-6xl text-[#4c260a]">O papel das ONGs</h2>
+  <div class="flex flex-col items-center my-20">
+    <img class="w-lg " src="/imagem_ongs.svg" alt="" />
+    <ul class="flex gap-20">
+      <li class="w-[50%]">
+        <p class="text-3xl my-10">
+          As ONGs desempenham um papel crucial na defesa dos direitos dos animais, atuando no
+          resgate, acolhimento, tratamento e adoção responsável de animais abandonados ou vítimas de
+          maus-tratos
+        </p>
+      </li>
+      <li class="w-[50%]">
+        <p class="text-3xl my-10">
+          Voluntários dedicam seu tempo para transformar a angústia em acolhimento. Com cuidado,
+          carinho e compromisso, eles reconquistam a confiança dos animais, oferecendo conforto,
+          amor, saúde e a chance de um novo lar. Após esse processo de recuperação, as ONGs
+          disponibilizam os pets para adoção, esperando que toquem os corações de quem busca um
+          verdadeiro companheiro.
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
-<style scoped>
-h2 {
-  text-align: center;
-  font-size: 500%;
-  color: #4c260a;
-  margin: 5% 0;
-}
-p {
-  font-size: 300%;
-  margin: 5% 0;
-}
-img {
-  width: 100%;
-  margin: auto;
-}
-.hidden {
-  opacity: 0;
-  transition: all 1s;
-  transform: translateY(10%);
-}
-.show {
-  opacity: 1;
-  transition: all 1s;
-  transform: translateY(0);
-}
-</style>
