91960f7ca94be71a8598c837fd153030c632ac91
AndreRicardoSc <andrenarutto7@gmail.com>
2025-08-13 15:57:28 -0300

    FEAT: Mount login page #12

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,11 +2,11 @@
 import LoginForm from '../components/LoginForm.vue';
 import LoginInstructions from '../components/LoginInstructions.vue';
 
-
 </script>
-<template>
-    <section class="flex justify-center gap-x-100">
-        <LoginInstructions/>
-        <LoginForm/>
+<template>  
+    <section class="flex justify-center gap-x-50 py-40 bg-[#03497B]">
+        <LoginInstructions class="z-10"/>
+        <LoginForm class="z-10"/>
     </section>
+    <img class="w-[100%] transform-[translateY(-80%)] bg-[#03497B] absolute z-0" src="/blue_waves.svg" alt="">
 </template>
