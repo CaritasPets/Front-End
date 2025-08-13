2d204a4edb87df22261f3b575b97c204ec03593d
AndreRicardoSc <andrenarutto7@gmail.com>
2025-08-13 15:50:49 -0300

    FEAT: Create login page components #12

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -32,24 +32,24 @@ const login = async () => {
 }
 </script>
 <template>
-  <form @submit.prevent="login" class="flex flex-col items-center gap-y-10">
+  <form @submit.prevent="login" class="flex flex-col items-center gap-y-12">
     <div class="flex flex-col gap-y-2">
-      <label class="text-2xl">Nome de usuário:</label>
-      <input class="text-2xl text-[#FDA202] border-2 border-[#FDA202] rounded-xl px-2 py-1"
+      <label class="text-2xl text-white">Nome de usuário:</label>
+      <input class="text-lg text-[#FDA202] border-2 border-[#FDA202] rounded-xl px-2 py-2 w-100"
         placeholder="Nome de usuário" 
         v-model="username" 
         required 
        />
     </div>
     <div class="flex flex-col gap-y-2">
-        <label class="text-2xl">Senha:</label>
-        <input class="text-2xl text-[#FF7700] border-2 border-[#FF7700] rounded-xl px-2 py-1"
+        <label class="text-2xl text-white">Senha:</label>
+        <input class="text-lg text-[#FF7700] border-2 border-[#FF7700] rounded-xl px-2 py-2 w-100"
             placeholder="Senha" 
             v-model="password" 
             type="password"
             required 
        />
     </div>
-    <button type="submit" class=" text-xl rounded-xl py-2 px-6 bg-[#FDA202] cursor-pointer">Fazer Login</button>
+    <button type="submit" class=" text-xl rounded-xl py-2 px-6 bg-[#FDA202] cursor-pointer border-2 border-transparent transition-all duration-500 hover:bg-transparent hover:border-[#FDA202] hover:text-[#FDA202]">Fazer Login</button>
   </form>
 </template>
