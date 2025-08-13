2cd0a3a8ff0dc41b61f10acc776472b2c11b52f5
AndreRicardoSc <andrenarutto7@gmail.com>
2025-08-13 15:59:08 -0300

    FEAT: Add the validateFooter in App vue #12

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <script setup>
 import FooterComponent from './components/FooterComponent.vue'
 import HeaderComponent from './components/HeaderComponent.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+function validateFooter(url) {
+  if(url.slice(0, 5) == '/user'){
+    return false
+  } else{
+    return true
+  }
+}
 </script>
 
 <template>
@@ -8,7 +18,7 @@ import HeaderComponent from './components/HeaderComponent.vue';
     <HeaderComponent/>
   </header>
   <RouterView />
-  <footer class="bg-[url(/footer.svg)] bg-no-repeat bg-size-[100%] py-96 ">
+  <footer v-if="validateFooter(route.path)" class="bg-[url(/footer.svg)] bg-no-repeat bg-size-[100%] py-96 ">
     <FooterComponent />
   </footer>
 </template>
