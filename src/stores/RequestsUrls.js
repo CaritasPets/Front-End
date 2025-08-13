24c2273478615e4491ff61f21b95a7e91bf285e5
AndreRicardoSc <andrenarutto7@gmail.com>
2025-08-13 15:59:41 -0300

    FEAT: Creating the requestUrlStore #12

--- src/stores/RequestsUrls.js
+++ src/stores/RequestsUrls.js
@@ -2,8 +2,18 @@ import { defineStore } from "pinia";
 
 export const useRequestUrlStore = defineStore('requestUrlStore', () => {
     const urls = {
+        //endpoints para puxar as informações que aparecerão no site
+        organizacoes: '/organizacoes',
+        perdidos: '/perdidos/',
+        pets: '/pets/',
+        portes: '/portes/',
+        racas: '/racas/',
+        //registro e autenticação de usuários
+        register: '/auth/register',
+        registerVoluntario: '/auth/register/voluntario',
+        registerOngs: 'auth/register/organizacao',
+        refresh: '/refresh/',
         token: '/token/',
-        organizations: '/organizacoes',
     }
 
     return (
