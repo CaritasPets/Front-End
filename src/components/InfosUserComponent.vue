e4146772c2ea43deea77e6e17d6e01e5b640cc3c
AndreRicardoSc <andrenarutto7@gmail.com>
2025-08-25 14:43:14 -0300

    FIX: Removing unnecessary code in InfosUserComponent #18

--- src/components/InfosUserComponent.vue
+++ src/components/InfosUserComponent.vue
@@ -1,43 +1,30 @@
+<script setup>
+const props = defineProps(['user'])
+</script>
 <template>
-  <section class=" text-white text-center p-8 rounded-xl">
-    <h2 class="text-3xl font-bold mb-6">{{ user.name }}</h2>
+  <section class="text-white text-center">
+    <div class="bg-[#03497B] pt-30">
+      <h2 class="text-6xl font-bold mb-6 z-10 relative">{{ props.user.nome }}</h2>
 
-    <div class="flex flex-col md:flex-row items-center justify-center gap-8">
-      <img :src="user.photo || fallbackPhoto"
-           alt="Foto do usuário"
-           class="w-36 h-36 rounded-xl object-cover bg-gray-300" />
+      <div class="flex flex-col md:flex-row items-center justify-center gap-8 z-10 relative">
+        <img :src="props.user.foto"
+            alt="Foto do usuário"
+            class="w-50 h-50 rounded-xl object-cover bg-gray-300" />
 
-      <ul class="text-left space-y-2">
-        <li><strong>Username:</strong> {{ '' }}</li>
-        <li><strong>Telefone:</strong> {{ '' }}</li>
-        <li><strong>CPF:</strong> {{ '' }}</li>
-        <li><strong>Data de Nascimento:</strong> {{ '' }}</li>
-        <li><strong>Email:</strong> {{'' }}</li>
-      </ul>
+        <ul class="text-left space-y-2">
+          <li class="text-2xl"><strong>Username:</strong> {{ props.user.username }}</li>
+          <li class="text-2xl"><strong>Telefone:</strong> {{ props.user.telefone }}</li>
+          <li class="text-2xl"><strong>CPF:</strong> {{ props.user.cpf }}</li>
+          <li class="text-2xl"><strong>Data de Nascimento:</strong> {{ props.user.data_nascimento }}</li>
+          <li class="text-2xl"><strong>Email:</strong> {{ props.user.email }}</li>
+        </ul>
+      </div>
+
+      <button class="mt-15 px-6 py-2 z-10 relative bg-yellow-500 text-black font-semibold rounded-lg border-2 border-transparent transition-all duration-500 cursor-pointer hover:bg-transparent hover:border-[#FDA202] hover:text-[#FDA202]">
+        Alterar informações
+      </button>
     </div>
-
-    <button class="mt-6 px-6 py-2 bg-yellow-500 text-black font-semibold rounded-lg hover:bg-yellow-400">
-      Alterar informações
-    </button>
+    <img src="/blue_waves.svg" alt="banner_waves" class="w-full transform-[translateY(-60%)] z-0 relative">
   </section>
 </template>
 
-<script setup>
-const props = defineProps({
-  user: {
-    type: Object,
-    default: () => ({
-      name: 'Nome do Usuário',
-      photo: '',
-      username: '',
-      phone: '',
-      cpf: '',
-      birthDate: '',
-      email: ''
-    })
-  }
-})
-
-const fallbackPhoto = 'https://via.placeholder.com/144'
-const { user } = props
-</script>
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -6,7 +6,17 @@ import PetsSavesComponent from '@/components/PetsSavesComponent.vue';
 </script>
 
 <template>
-  <InfosUserComponent/>
+  <InfosUserComponent
+    :user="{
+      nome: 'Breno',
+      foto: '',
+      username: 'breno_rohregger',
+      telefone: '47984542516',
+      cpf: '11251526993',
+      data_nascimento: '04/05/2008',
+      email: 'brenoot0405@gmail.com'
+    }"
+  />
   <OngsSavesComponent/>
   <PetsSavesComponent/>
 </template>
