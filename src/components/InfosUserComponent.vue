202191dc2ebad0a5ad85a66876468c1a37342eaa
brenorohregger <brenoot0405@gmail.com>
2025-08-27 16:53:33 -0300

    FEAT: adjusted information #18

--- src/components/InfosUserComponent.vue
+++ src/components/InfosUserComponent.vue
@@ -3,17 +3,20 @@ const props = defineProps(['user'])
 </script>
 
 <template>
-  <section class="text-white text-center">
+  <section class="text-white">
     <div class="bg-[#03497B] pt-30">
-      <h2 class="text-6xl font-bold mb-6 z-10 relative">{{ props.user.nome }}</h2>
 
-      <div class="flex flex-col md:flex-row items-center md:items-start justify-center md:justify-start gap-16 z-10 relative max-w-6xl mx-auto">
+      <div class="flex flex-col md:flex-row items-center md:items-center justify-center md:justify-start gap-16 z-10 relative max-w-6xl mx-auto">
+
+        <div class="flex flex-col items-center">
+
+          <h2 class="text-6xl font-bold mb-6">{{ props.user.nome }}</h2>
 
 
-        <img :src="props.user.foto"
-             alt="Foto do usuário"
-             class="w-80 h-80 rounded-4xl object-cover bg-gray-300 shadow-lg" />
-
+          <img :src="props.user.foto"
+               alt=""
+               class="w-100 h-100 rounded-[60px] object-cover bg-gray-300 shadow-lg" />
+        </div>
 
         <ul class="text-left space-y-7">
           <li class="text-2xl"><strong>Username:</strong> {{ props.user.username }}</li>
@@ -24,10 +27,14 @@ const props = defineProps(['user'])
         </ul>
       </div>
 
-      <button class="mt-12 px-6 py-2 z-10 relative bg-yellow-500 text-black font-semibold rounded-lg border-2 border-transparent transition-all duration-500 cursor-pointer hover:bg-transparent hover:border-[#FDA202] hover:text-[#FDA202]">
-        Alterar informações
-      </button>
+
+      <div class="flex justify-center">
+        <button class="mt-12 px-6 py-2 bg-yellow-500 text-black font-semibold rounded-lg border-2 border-transparent transition-all duration-500 cursor-pointer hover:bg-transparent hover:border-[#FDA202] hover:text-[#FDA202]">
+          Alterar informações
+        </button>
+      </div>
     </div>
-    <img src="/blue_waves.svg" alt="banner_waves" class="w-full transform-[translateY(-20%)] z-0 relative">
+
+    <img src="/blue_waves.svg" alt="banner_waves" class="w-full transform translate-y-[-20%] z-0 relative">
   </section>
 </template>
