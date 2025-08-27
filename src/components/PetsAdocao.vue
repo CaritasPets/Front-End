a6d75bb4dcaebfabdb3f4a5d370176c81609c004
Johann5731 <johannmborges27@gmail.com>
2025-08-27 16:47:08 -0300

    FEAT: Small Fixes #9

--- src/components/PetsAdocao.vue
+++ src/components/PetsAdocao.vue
@@ -1,15 +1,18 @@
 <script setup>
-  const props = defineProps(["foto", "nome", "genero"]);
-  const backgrounds = ["#03497B", "#587911", "#FDA202"];
-  function sortBackground (){
-      const randomNum = Math.floor(Math.random() * 3);
-      return backgrounds[randomNum];
-  }
+const props = defineProps(["foto", "nome", "genero"]);
+const backgrounds = ["#03497B", "#587911", "#FDA202"];
+function sortBackground() {
+  const randomNum = Math.floor(Math.random() * 3);
+  return backgrounds[randomNum];
+}
 </script>
 <template>
-  <div class=" flex flex-col items-center w-[15%] rounded-4xl mx-7 mb-14 px-5 shadow-[7px_7px_2px_rgba(0,0,0,0.30)]" :style="`background: ${sortBackground()}`">
-    <img class=" w-60 rounded-4xl mt-6 mb-4 " :src="props.foto" :alt="`${props.nome}-image`">
-    <p class="my-1 text-[1.5rem] text-white">{{ props.nome }}</p>
-    <p class="my-1 text-[1.5rem] text-white -mt-2 mb-5.5">{{ props.genero }}</p>
+  <div class=" flex flex-col items-center w-[15%] rounded-4xl mx-7 mb-14 px-5 shadow-[7px_7px_2px_rgba(0,0,0,0.30)]"
+    :style="`background: ${sortBackground()}`">
+    <a href="" class=" flex flex-col items-center">
+      <img class=" w-60 rounded-4xl mt-6 mb-4 " :src="props.foto" :alt="`${props.nome}-image`">
+      <p class="my-1 text-[1.5rem] text-white">{{ props.nome }}</p>
+      <p class="my-1 text-[1.5rem] text-white -mt-2 mb-5.5">{{ props.genero }}</p>
+    </a>
   </div>
 </template>
