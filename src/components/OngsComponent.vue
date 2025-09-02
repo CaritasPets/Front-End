53a1b2ce7d1415921418e6c25b68ab9d7f030142
brenorohregger <brenoot0405@gmail.com>
2025-09-02 16:44:44 -0300

    FEAT: Responsive header completed #25

--- src/components/OngsComponent.vue
+++ src/components/OngsComponent.vue
@@ -6,7 +6,7 @@
         return backgrounds[randomNum];
     }
 </script>
-<template>    
+<template>
     <div class="flex flex-col items-center p-10 w-[calc(100%/5)] my-2 mx-1 text-center rounded-4xl cursor-pointer transition-all duration-500 hover:opacity-60" :style="`background: ${sortBackground()}`">
         <img class="rounded-full w-[100%]" :src="props.logo" :alt="`${props.nome}-logo`">
         <p class="text-white text-2xl">{{ props.nome }}</p>
