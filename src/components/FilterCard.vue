be0435f895e114bfcec1176939ea88a9566e7c90
AndreRicardoSc <andrenarutto7@gmail.com>
2025-09-02 07:25:53 -0300

    FEAT: Adding FilterCard component #9

---
+++ src/components/FilterCard.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+
+    const props = defineProps(['opcoes'])
+    const open = ref(false)
+
+    //exemplo de objeto a ser usado
+    /*const propriedades = [
+        {
+            nomePai: 'Especie',
+            nomesFilhos: [
+                {
+                    nome: 'Gato',
+                    valor:'gato'
+                }
+            ]
+        }
+    ]*/
+</script>
+<template>
+    <div v-for="opcao of props.opcoes" :key="opcao">
+        <div>
+            <h3>{{ opcao.nomePai }}</h3>
+            <span></span>
+        </div>
+        <div v-if="open">
+            <label v-for="nomeFilho of opcoes.nomesFilhos" :key="nomeFilho">
+                <input type="checkbox" :value="nomeFilho.valor">
+                {{ nomeFilho.nome }}
+            </label>
+        </div>
+    </div>
+</template>
--- src/components/FilterComponent.vue
+++ src/components/FilterComponent.vue
@@ -1,23 +1,31 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
+import FilterCard from './FilterCard.vue';
 
 const open = ref(false);
+const openFilter = ref(false);
+
 </script>
 <template>
-  <section class="flex flex-col items-center">
-    <h2 class=" text-7xl text-center mb-25">Pets para Adoção</h2>
-    <button class=" text-5xl rounded-4xl bg-[#FFE078] px-13 py-3 cursor-pointer" @click="open = !open">
-      <div class="flex gap-x-5">
+  <section class="flex flex-col items-center bg-amber-300">
+    <h2 class="text-7xl text-center mb-25">Pets para Adoção</h2>
+    <div class="flex flex-col items-center w-full">
+      <button
+        class="flex gap-4 text-5xl rounded-4xl bg-[#FFE078] px-13 py-3 cursor-pointer"
+        @click="open = !open"
+      >
         <p>Filtros</p>
         <span class="mdi mdi-filter-outline"></span>
-      </div>
-      <div class="" v-if="open">
-        <div>
-          <fieldset >
-            <input type="checkbox" name="" id="">
-          </fieldset>
+      </button>
+      <div v-if="open" class="bg-amber-400 w-[70%] rounded-4xl">
+        <div @click="openFilter = !openFilter" class="flex cursor-pointer">
+          <h3>Abrir</h3>
+          <span class="mdi mdi-arrow-down"></span>
+        </div>
+        <div class="" v-if="openFilter">
+          <FilterCard/>
         </div>
       </div>
-    </button>
+    </div>
   </section>
 </template>
