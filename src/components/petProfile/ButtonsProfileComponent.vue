7a6b6b25e6144bb04fda83f50cc6474f96e6293f
AndreRicardoSc <andrenarutto7@gmail.com>
2025-09-04 21:16:39 -0300

    FEAT: Created buttonProfileComponent #23

---
+++ src/components/petProfile/ButtonsProfileComponent.vue
@@ -0,0 +1,16 @@
+<script setup>
+    const props = defineProps(['objButtons']);
+</script>
+<template>
+    <section class="flex justify-center gap-5">
+        <RouterLink  v-for="button of props.objButtons"
+        class="text-xl rounded-xl py-1 px-3"
+        :style="`background: ${button.background}`"
+        :key="button.text" 
+        :to="button.link"
+        >
+            <span :class="button.icon"></span>
+            {{ button.text }}
+        </RouterLink>
+    </section>
+</template>
