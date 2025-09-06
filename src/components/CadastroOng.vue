70366bd1534cfa443030bc4ca7648c2f4a3749c1
Johann5731 <johannmborges27@gmail.com>
2025-09-06 11:05:03 -0300

    FEAT:  Construction of the NGO registration page started #37

---
+++ src/components/CadastroOng.vue
@@ -0,0 +1,33 @@
+<script computed>
+
+</script>
+<template>
+  <form action="">
+    <div>
+      <div class="">
+        <label class="">*Nome:</label>
+        <input class="" placeholder="Nome." v-model="username" required />
+        <label class="">*Telefone:</label>
+        <input class="" placeholder="Telefone." v-model="" type="number" required />
+        <label class="">*Email:</label>
+        <input class="" placeholder="Email." v-model="email" type="email" required />
+        <label class="">CNPJ:</label>
+        <input class="" placeholder="CNPJ (se possui)." v-model="username" required />
+        <label class="">Instagram:</label>
+        <input class="" placeholder="Instagram (se possui)." v-model="username" required />
+      </div>
+      <div>
+        <label class="">Facebook:</label>
+        <input class="" placeholder="Facebook (se possui)." v-model="username" required />
+        <label class="">*Senha:</label>
+        <input class="" placeholder="........" v-model="username" required />
+        <label class="">Endereço:</label>
+        <input class="" placeholder="Endereço da ONG (se possui)." v-model="username" required />
+        <label class="">Foto de Perfil:</label>
+        <input class="" placeholder="Nome." v-model="username" required />
+        <label class="">Descrição:</label>
+        <input class="" placeholder="Descrição." v-model="username" required />
+      </div>
+    </div>
+  </form>
+</template>
