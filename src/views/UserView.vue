4c332f92ebc10fd007c3d040ab6594962fb575e6
brenorohregger <brenoot0405@gmail.com>
2025-09-06 09:49:46 -0300

    FEAT:  fixing rescued pets #18

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -2,6 +2,7 @@
 import InfosUserComponent from '@/components/InfosUserComponent.vue';
 import OngsComponent from '@/components/OngsComponent.vue';
 import PetsSavesComponent from '@/components/PetsSavesComponent.vue';
+
 const ongsFavorite = [
   {
     id: 1,
@@ -24,30 +25,11 @@ const ongsFavorite = [
     logo: '/logo.svg'
   }
 ]
+
+
 const petsFavorite = [
-  {
-    id: 1,
-    nome: 'cachorro',
-    fotoPet: '/Logo.svg',
-  },
-  {
-    id: 1,
-    nome: 'cachorro',
-    fotoPet: '/Logo.svg',
-  },
-  {
-    id: 1,
-    nome: 'cachorro',
-    fotoPet: '/Logo.svg',
-  },
-  {
-    id: 1,
-    nome: 'cachorro',
-    fotoPet: '/Logo.svg',
-  }
 ]
 </script>
-
 <template>
   <InfosUserComponent
     :user="{
@@ -60,17 +42,21 @@ const petsFavorite = [
       email: 'brenoot0405@gmail.com'
     }"
   />
-  <h2 class="text-center text-5xl text-[#4c260a]">Ongs Salvas</h2>
+  <h2 class="text-center text-5xl text-[#4c260a]">Seus Pets salvos:</h2>
+ <div class="flex flex-wrap justify-between mt-20 px-60">
+    <PetsSavesComponent
+      v-for="pet in petsFavorite"
+      :key="pet.id"
+      :fotoPet="pet.fotoPet"
+      :nome="pet.nome"
+    />
+  </div>
+  <h2 class="text-center text-5xl text-[#4c260a]">Suas ONGs Salvas:</h2>
   <div class="flex flex-wrap justify-between mt-20 px-60">
     <OngsComponent v-for="ong of ongsFavorite" :key="ong.id"
       :logo="ong.logo"
       :nome="ong.nome"
     />
   </div>
-  <div class="flex flex-wrap justify-between mt-20 px-60">
-  <PetsSavesComponent v-for="pet of petsFavorite" :key="pet.id"
-      :fotoPet="pet.fotoPet"
-      :nome="pet.nome"
-      />
-  </div>
+
 </template>
