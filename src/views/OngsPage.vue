cf6a6206fb926d57b9b10988b852874c90964871
AndreRicardoSc <andrenarutto7@gmail.com>
2025-09-11 15:56:40 -0300

    FEAT: Create the Error component #48

--- src/views/OngsPage.vue
+++ src/views/OngsPage.vue
@@ -3,6 +3,7 @@ import { onMounted } from 'vue';
 import BannerComponent from '../components/BannerComponent.vue';
 import PapelOngs from '../components/OngComponent/PapelOngs.vue';
 import OngsComponent from '../components/OngsComponent.vue';
+import ErrorComponent from '../components/Errors/ErrorComponent.vue';
 
 import { useOngService } from '../services/ongs/ongService';
 const ongService = useOngService();
@@ -31,10 +32,17 @@ onMounted(() => {
   </section>
   <section class="m-40">
     <h2 class="text-center text-5xl text-[#4c260a]">ONGs</h2>
-    <div class="flex flex-wrap justify-between mt-20">
-      <OngsComponent v-for="ong of ongStore.propriedades" :key="ong.id" 
+    <div v-if="!ongService.inError" class="flex flex-wrap justify-between mt-20">
+      <OngsComponent 
+        v-for="ong of ongStore.propriedades" :key="ong.id" 
         :logo="'/logo.svg'"
         :nome="ong.nome"
+      />
+    </div>
+    <div v-else>
+      <ErrorComponent
+        :status-error="ongService.statusError"
+        :message="ongService.statusText"
       />
     </div>
   </section>
