e39e2e1ff4734555b898ee68cdf51e0c339c4440
Johann5731 <johannmborges27@gmail.com>
2025-09-12 06:05:16 -0300

    FEAT: Small improvements #37

--- src/components/OngForm.vue
+++ src/components/OngForm.vue
@@ -19,14 +19,14 @@ function onFileChange(event) {
 }
 </script>
 <template>
-  <form action="" class="">
-    <div class="flex">
+  <form action="" class="relative z-10">
+    <div class="flex justify-around">
       <div class="">
         <ul>
           <li class="flex flex-col gap-y-2">
-            <label class="text-2xl text-[#1E0B00]">*Nome:</label>
+            <label class="text-3xl text-[#1E0B00]">*Nome:</label>
             <input
-              class="text-2xl text-[#03497B] py-1 px-2 my-2 border-2 rounded-xl w-120"
+              class="text-2xl text-[#03497B] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-150"
               placeholder="Nome."
               v-model="nome"
               type="text"
@@ -34,9 +34,9 @@ function onFileChange(event) {
             />
           </li>
           <li class="flex flex-col gap-y-2">
-            <label class="text-2xl text-[#1E0B00]">*Telefone:</label>
+            <label class="text-3xl text-[#1E0B00]">*Telefone:</label>
             <input
-              class="text-2xl text-[#FF7700] py-1 px-2 my-2 border-2 rounded-xl w-120"
+              class="text-2xl text-[#FF7700] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-150"
               placeholder="Telefone."
               v-model="telefone"
               type="text"
@@ -44,9 +44,9 @@ function onFileChange(event) {
             />
           </li>
           <li class="flex flex-col gap-y-2">
-            <label class="text-2xl text-[#1E0B00]">*Email:</label>
+            <label class="text-3xl text-[#1E0B00]">*Email:</label>
             <input
-              class="text-2xl text-[#FDA202] py-1 px-2 my-2 border-2 rounded-xl w-120"
+              class="text-2xl text-[#FDA202] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-150"
               placeholder="Email."
               v-model="email"
               type="email"
@@ -54,18 +54,18 @@ function onFileChange(event) {
             />
           </li>
           <li class="flex flex-col gap-y-2">
-            <label class="text-2xl text-[#1E0B00]">CNPJ:</label>
+            <label class="text-3xl text-[#1E0B00]">CNPJ:</label>
             <input
-              class="text-2xl text-[#104C00] py-1 px-2 my-2 border-2 rounded-xl w-120"
+              class="text-2xl text-[#104C00] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-150"
               placeholder="CNPJ (se possui)."
               v-model="cnpj"
               type="text"
             />
           </li>
           <li class="flex flex-col gap-y-2">
-            <label class="text-2xl text-[#1E0B00]">Instagram:</label>
+            <label class="text-3xl text-[#1E0B00]">Instagram:</label>
             <input
-              class="text-2xl text-[#03497B] py-1 px-2 my-2 border-2 rounded-xl w-120"
+              class="text-2xl text-[#03497B] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-150"
               placeholder="Instagram (se possui)."
               v-model="instagram"
               type="text"
@@ -76,18 +76,18 @@ function onFileChange(event) {
       <div>
         <ul>
           <li class="flex flex-col gap-y-2">
-            <label class="text-2xl text-[#1E0B00]">Facebook:</label>
+            <label class="text-3xl text-[#1E0B00]">Facebook:</label>
             <input
-              class="text-2xl text-[#FDA202] py-1 px-2 my-2 border-2 rounded-xl w-120"
+              class="text-2xl text-[#FDA202] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-150"
               placeholder="Facebook (se possui)."
               v-model="facebook"
               type="text"
             />
           </li>
           <li class="flex flex-col gap-y-2">
-            <label class="text-2xl text-[#1E0B00]">*Senha:</label>
+            <label class="text-3xl text-[#1E0B00]">*Senha:</label>
             <input
-              class="text-2xl text-[#104C00] py-1 px-2 my-2 border-2 rounded-xl w-120"
+              class="text-2xl text-[#104C00] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-150"
               placeholder="........"
               v-model="password"
               type="password"
@@ -95,20 +95,20 @@ function onFileChange(event) {
             />
           </li>
           <li class="flex flex-col gap-y-2">
-            <label class="text-2xl text-[#1E0B00]">Endereço:</label>
+            <label class="text-3xl text-[#1E0B00]">Endereço:</label>
             <input
-              class="text-2xl text-[#9EAF50] py-1 px-2 my-2 border-2 rounded-xl w-120"
+              class="text-2xl text-[#9EAF50] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-150"
               placeholder="Endereço da ONG (se possui)."
               v-model="endereco"
               type="text"
             />
           </li>
           <li class="flex flex-col">
-            <p class="text-2xl text-[#1E0B00]">Foto de Perfil</p>
+            <p class="text-3xl text-[#1E0B00]">Foto de Perfil</p>
             <input id="fileInput" class="hidden" type="file" @change="onFileChange" />
             <label
             for="fileInput"
-            class="text-2xl text-[#FF7700] border-2 py-1 px-2 my-2 rounded-xl w-120 cursor-pointer"
+            class="text-2xl text-[#FF7700] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-150 cursor-pointer"
             >Selecionar Arquivo</label>
             <div v-if="foto" class="text-xl text-[#FF7700]">
               <p>Arquivo para foto selecionado:</p>
@@ -116,9 +116,9 @@ function onFileChange(event) {
             </div>
           </li>
           <li class="flex flex-col">
-            <label class="text-2xl text-[#1E0B00]">Descrição:</label>
+            <label class="text-3xl text-[#1E0B00]">Descrição:</label>
             <input
-              class="text-2xl text-[#FDA202] py-1 px-2 my-2 border-2 rounded-xl w-120"
+              class="text-2xl text-[#FDA202] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-150"
               placeholder="Descrição."
               v-model="username"
               type="text"
--- src/views/CadastroOng.vue
+++ src/views/CadastroOng.vue
@@ -4,10 +4,10 @@ import OngForm from '../components/OngForm.vue'
 <template>
   <section class="bg-[#FFF493] pt-20 px-10">
     <div class="z-10 relative pl-15 mb-20">
-      <h2 class="text-6xl text-[#1E0B00]">Cadastro de ONG</h2>
+      <h2 class="text-7xl text-[#1E0B00]">Cadastro de ONG</h2>
       <p class="text-3xl text-[#1E0B00] font-light">Preencha as informações de cadastro:</p>
     </div>
-    <OngForm class="z-10 relative" />
+    <OngForm class="pl-15 z-10 relative" />
   </section>
   <img
     class="w-[100%] transform-[translateY(-50%)] bg-[#FFF493] absolute z-0"
