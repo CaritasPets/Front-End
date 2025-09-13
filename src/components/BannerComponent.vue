34dcea030329fac7114fe3e5fff884b421f895ac
AmandaRibeiroLuz <amanda.ribeiro.da.luz.201@gmail.com>
2025-09-12 22:24:59 -0300

    FEAT: Responsiveness applied on filterComponent. #47

--- src/components/BannerComponent.vue
+++ src/components/BannerComponent.vue
@@ -9,16 +9,16 @@ const props = defineProps(['background', 'image', 'title', 'text', 'waves', 'but
     <div class="text-white relative z-10 flex flex-col items-center gap-y-4 sm:items-baseline">
 
 
-      <h1 class="text-5xl text-center font-[Handlee] mt-10 mb-5 sm:mt-25 sm:mb-15 sm:text-6xl lg:text-7xl sm:text-left">{{ props.title }}</h1>
+      <h1 class="text-4xl text-center font-[Handlee] mt-10 mb-5 sm:mt-25 sm:mb-15 sm:text-6xl lg:text-7xl sm:text-left">{{ props.title }}</h1>
       <p class="text-2xl text-rigth mx-10 text-center sm:text-left">{{ props.text }}</p>
-      <RouterLink :to="props.buttonLink" v-if="props.buttonBoolean" class="bg-[#03497B] mt-10 w-70 pl-15 pt-2 pb-2 rounded-2xl font-[Sen]">
+      <RouterLink :to="props.buttonLink" v-if="props.buttonBoolean" class="bg-[#03497B] mt-10 w-70 pl-10 text-lg pt-2 pb-2 rounded-2xl font-[Sen]">
         <button>
           {{ props.buttonText }}
         </button>
       </RouterLink>
     </div>
     <div>
-      <img class="z-10 relative" :src="props.image" alt="banner">
+      <img class="z-10 relative " :src="props.image" alt="banner">
     </div>
   </div>
   <div>
