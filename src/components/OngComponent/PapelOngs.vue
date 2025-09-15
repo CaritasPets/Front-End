5b257eb6c74c6e379a8cc514c8f0ffb656b1e54d
AmandaRibeiroLuz <amanda.ribeiro.da.luz.201@gmail.com>
2025-09-14 21:37:50 -0300

    FEAT: Starting to wapply responsiveness on ONGsPage. #33

--- src/components/OngComponent/PapelOngs.vue
+++ src/components/OngComponent/PapelOngs.vue
@@ -1,13 +1,13 @@
 <template>
   <h2 class="text-center text-6xl text-[#4c260a] font-['Handlee']">O papel das ONGs</h2>
-    <p class="text-3xl my-10 text-justify font-['Sen']">
+    <p class="text-2xl my-10 text-justify font-['Sen'] md:text-3xl">
       As ONGs desempenham um papel crucial na defesa dos direitos dos animais, atuando no resgate, acolhimento, tratamento e adoção responsável de animais abandonados ou vítimas de maus-tratos. Além disso, promovem campanhas de conscientização sobre posse responsável e combate ao abandono.
     </p>
-    <p class="text-3xl my-10 text-justify font-['Sen']">
+    <p class="text-2xl my-10 text-justify font-['Sen'] md:text-3xl">
       Com o apoio da comunidade, essas instituições transformam vidas e constroem uma sociedade mais justa e solidária.
     </p>
     <img class="w-165 mx-auto mt-20" src="/imagem_ongs.svg" alt="Imagem de voluntários cuidando de um cachorro" />
-    <p class="text-3xl my-10 text-justify font-['Sen']">
+    <p class="text-2xl my-10 text-justify font-['Sen'] md:text-3xl">
       Voluntários dedicam seu tempo para transformar a angústia em acolhimento. Com cuidado, carinho e compromisso, eles reconquistam a confiança dos animais, oferecendo conforto, amor, saúde e a chance de um novo lar. Após esse processo de recuperação, as ONGs disponibilizam os pets para adoção, esperando que toquem os corações de quem busca um verdadeiro companheiro.
     </p>
 </template>
