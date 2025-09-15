84d7dbb04036aa426aa6c8fae77457548220d96f
AmandaRibeiroLuz <amanda.ribeiro.da.luz.201@gmail.com>
2025-09-15 11:10:50 -0300

    FEAT: Responsiveness of AboutPage applied. #55

--- src/components/AboutComponents/QuemSomosComponent.vue
+++ src/components/AboutComponents/QuemSomosComponent.vue
@@ -1,15 +1,15 @@
 <script setup></script>
 
 <template>
-    <h2 class="text-center text-6xl text-[#4c260a]">Quem somos?</h2>
-    <p class= "text-2xl my-10 mt-20 text-justify">
+    <h2 class="text-center text-5xl text-[#4c260a] font-[Handlee] md:text-6xl">Quem somos?</h2>
+    <p class= "text-xl my-10 mt-10 text-justify font-[Sen] lg:text-2xl lg:mt-20">
         Somos estudantes do Ensino Médio Integrado ao curso técnico em Informática para Internet, do Instituto Federal Catarinense, Campus Araquari.
     </p>
-    <div class="flex justify-between items-center">
+    <div class="block items-center lg:flex lg:justify-between">
         <p>
             <img class="w-180 mx-auto" src="/img_QuemSomos.svg" alt="equipe"/>
         </p>
-        <p class= "text-2xl my-10 text-justify">
+        <p class= "text-xl my-10 text-justify font-[Sen] lg:text-2xl">
             A nossa equipe é formada por: <br>
             Amanda Ribeiro da Luz, <br>
             André Ricardo Schultz, <br>
