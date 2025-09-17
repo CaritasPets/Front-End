9686b7de1bf87127e9e84f359be6018ad840f100
AmandaRibeiroLuz <amanda.ribeiro.da.luz.201@gmail.com>
2025-09-16 22:25:28 -0300

    FEAT: Responsiveness applied on ONG registration page. #71

--- src/components/OngForm.vue
+++ src/components/OngForm.vue
@@ -7,7 +7,7 @@ const email = ref('')
 const cnpj = ref('')
 const instagram = ref('')
 const facebook = ref('')
-const endereco = ref('')
+const endereco = ref('') 
 const password = ref('')
 const foto = ref(null)
 const descricao = ref('')
@@ -34,13 +34,13 @@ function resetForm() {
 </script>
 <template>
   <form action="" class="relative z-10" @reset.prevent="resetForm">
-    <div class="flex justify-around">
+    <div class="flex flex-col lg:flex-row lg:justify-around">
       <div class="">
         <ul>
           <li class="flex flex-col gap-y-2">
-            <label class="text-3xl text-[#1E0B00]">*Nome:</label>
+            <label class="text-xl sm:text-3xl text-[#1E0B00]">*Nome:</label>
             <input
-              class="text-2xl bg-[#FFF493] text-[#03497B] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-150"
+              class="text-xl sm:text-3xl bg-[#FFF493] text-[#03497B] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-full md:w-135"
               placeholder="Nome."
               v-model="nome"
               type="text"
@@ -48,9 +48,9 @@ function resetForm() {
             />
           </li>
           <li class="flex flex-col gap-y-2">
-            <label class="text-3xl text-[#1E0B00]">*Telefone:</label>
+            <label class="text-xl sm:text-3xl text-[#1E0B00]">*Telefone:</label>
             <input
-              class="text-2xl bg-[#FFF493] text-[#FF7700] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-150"
+              class="text-xl sm:text-3xl bg-[#FFF493] text-[#FF7700] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-full md:w-135"
               placeholder="Telefone."
               v-model="telefone"
               type="text"
@@ -58,9 +58,9 @@ function resetForm() {
             />
           </li>
           <li class="flex flex-col gap-y-2">
-            <label class="text-3xl text-[#1E0B00]">*Email:</label>
+            <label class="text-xl sm:text-3xl text-[#1E0B00]">*Email:</label>
             <input
-              class="text-2xl bg-[#FFF493] text-[#FDA202] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-150"
+              class="text-xl sm:text-3xl bg-[#FFF493] text-[#FDA202] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-full md:w-135"
               placeholder="Email."
               v-model="email"
               type="email"
@@ -68,18 +68,18 @@ function resetForm() {
             />
           </li>
           <li class="flex flex-col gap-y-2">
-            <label class="text-3xl text-[#1E0B00]">CNPJ:</label>
+            <label class="text-xl sm:text-3xl text-[#1E0B00]">CNPJ:</label>
             <input
-              class="text-2xl bg-[#FFF493] text-[#104C00] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-150"
-              placeholder="CNPJ (se possui)."@reset.prevent="resetForm"
+              class="text-xl sm:text-3xl bg-[#FFF493] text-[#104C00] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-full md:w-135"
+              placeholder="CNPJ (se possui)."
               v-model="cnpj"
               type="text"
             />
           </li>
           <li class="flex flex-col gap-y-2">
-            <label class="text-3xl text-[#1E0B00]">Instagram:</label>
+            <label class="text-xl sm:text-3xl text-[#1E0B00]">Instagram:</label>
             <input
-              class="text-2xl bg-[#FFF493] text-[#03497B] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-150"
+              class="text-xl sm:text-3xl bg-[#FFF493] text-[#03497B] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-full md:w-135"
               placeholder="Instagram (se possui)."
               v-model="instagram"
               type="text"
@@ -87,21 +87,21 @@ function resetForm() {
           </li>
         </ul>
       </div>
-      <div>
+      <div class="w-[]">
         <ul>
           <li class="flex flex-col gap-y-2">
-            <label class="text-3xl text-[#1E0B00]">Facebook:</label>
+            <label class="text-xl sm:text-3xl text-[#1E0B00]">Facebook:</label>
             <input
-              class="text-2xl bg-[#FFF493] text-[#FDA202] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-150"
+              class="text-xl sm:text-3xl bg-[#FFF493] text-[#FDA202] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-full md:w-135"
               placeholder="Facebook (se possui)."
               v-model="facebook"
               type="text"
             />
           </li>
           <li class="flex flex-col gap-y-2">
-            <label class="text-3xl text-[#1E0B00]">*Senha:</label>
+            <label class="text-xl sm:text-3xl text-[#1E0B00]">*Senha:</label>
             <input
-              class="text-2xl bg-[#FFF493] text-[#104C00] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-150"
+              class="text-xl sm:text-3xl bg-[#FFF493] text-[#104C00] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-full md:w-135"
               placeholder="........"
               v-model="password"
               type="password"
@@ -109,32 +109,32 @@ function resetForm() {
             />
           </li>
           <li class="flex flex-col gap-y-2">
-            <label class="text-3xl text-[#1E0B00]">Endereço:</label>
+            <label class="text-xl sm:text-3xl text-[#1E0B00]">Endereço:</label>
             <input
-              class="text-2xl bg-[#FFF493] text-[#9EAF50] py-2 px-4 my-2 gap-y-2 border-x-6 border-y-3 rounded-2xl w-150"
+              class="text-xl sm:text-3xl bg-[#FFF493] text-[#03497B] py-2 px-4 my-2 gap-y-2 border-x-6 border-y-3 rounded-2xl w-full md:w-135"
               placeholder="Endereço da ONG (se possui)."
               v-model="endereco"
               type="text"
             />
           </li>
           <li class="flex flex-col gap-y-2">
-            <p class="text-3xl text-[#1E0B00]">Foto de Perfil</p>
+            <p class="text-xl sm:text-3xl text-[#1E0B00]">Foto de Perfil</p>
             <input id="fileInput" class="hidden" type="file" @change="onFileChange" />
             <label
             for="fileInput"
-            class="text-2xl bg-[#FFF493] text-[#FF7700] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-150 cursor-pointer">
+            class="text-xl sm:text-3xl bg-[#FFF493] text-[#FF7700] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-full md:w-135 cursor-pointer">
             Selecionar Arquivo
             <span class="m-80 mdi mdi-folder-plus-outline"></span>
             </label>
-            <div v-if="foto" class="text-xl text-[#FF7700]">
+            <div v-if="foto" class="text-base sm:text-xl text-[#FF7700]">
               <p>Arquivo para foto selecionado:</p>
               <p>{{ foto.name }}</p>
             </div>
           </li>
           <li class="flex flex-col gap-y-2">
-          <label class="text-3xl text-[#1E0B00]">Descrição:</label>
+          <label class="text-xl sm:text-3xl text-[#1E0B00]">Descrição:</label>
           <input
-            class="text-2xl bg-[#FFF493] text-[#FDA202] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-150"
+            class="text-xl sm:text-3xl bg-[#FFF493] text-[#FDA202] py-2 px-4 my-2 border-x-6 border-y-3 rounded-2xl w-full md:w-135"
             placeholder="Descrição."
             v-model="descricao"
             type="text"
@@ -143,10 +143,10 @@ function resetForm() {
         </ul>
       </div>
     </div>
-    <div class="ml-36 my-14">
-      <h2 class="text-2xl">* Indica campo obrigatório.</h2>
+    <div class="ml-4 sm:ml-36 my-14">
+      <h2 class="text-xl sm:text-3xl">* Indica campo obrigatório.</h2>
     </div>
-    <div class="flex justify-center gap-30">
+    <div class="flex justify-center gap-4 sm:gap-30">
       <button
         class="text-xl rounded-xl py-2 pl-4 pr-6 bg-[#FDA202] cursor-pointer border-2 border-transparent transition-all duration-500 hover:bg-transparent hover:border-[#FDA202] hover:text-[#FDA202]"
         type="reset"
