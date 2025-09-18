64e39a43b366795b1aaed4481fc920abdb1fe7ea
AmandaRibeiroLuz <amanda.ribeiro.da.luz.201@gmail.com>
2025-09-18 12:49:15 -0300

    FEAT: Adoption pets registred. #94

--- src/stores/PetStore.js
+++ src/stores/PetStore.js
@@ -20,22 +20,24 @@ export const usePetStore = defineStore('petStore', () => {
             nome: 'Rex',
             genero: 'macho',
             foto: '/pets_adocao_images/pet1.jpg',
-            especie: 'cachorro',
+            especie: 'gato',
             vacinado: 'sim',
-            porte: 'medio',
+            porte: 'pequeno',
             castrado: 'sim',
             pelagem: 'curta'
         },
         {
             id: 2,
-            nome: 'Mia',
-            genero: 'femea',
-            foto: '/pets_adocao_images/pet2.jpg',
-            especie: 'gato',
-            vacinado: 'nao',
+            nome: 'Simba',
+            genero: 'macho',
+            foto: '/pets_adocao_images/pet19.jpg',
+            especie: 'outros',
+            vacinado: 'sim',
             porte: 'pequeno',
-            castrado: 'nao',
-            pelagem: 'longa'
+            castrado: 'sim',
+            pelagem: 'curta',
+            raca: 'Não possui'
+            
         },
         {
             id: 3,
@@ -44,20 +46,22 @@ export const usePetStore = defineStore('petStore', () => {
             foto: '/pets_adocao_images/pet3.jpg',
             especie: 'cachorro',
             vacinado: 'parcialmente',
-            porte: 'grande',
+            porte: 'medio',
             castrado: 'sim',
-            pelagem: 'media'
+            pelagem: 'media',
+            raca: 'Shitzu'
         },
         {
             id: 4,
             nome: 'Luna',
             genero: 'femea',
             foto: '/pets_adocao_images/pet4.jpg',
-            especie: 'gato',
+            especie: 'cachorro',
             vacinado: 'sim',
-            porte: 'pequeno',
+            porte: 'grande',
             castrado: 'nao',
-            pelagem: 'curta'
+            pelagem: 'curta',
+            raca: 'Não possui'
         },
         {
             id: 5,
@@ -66,9 +70,10 @@ export const usePetStore = defineStore('petStore', () => {
             foto: '/pets_adocao_images/pet5.jpg',
             especie: 'cachorro',
             vacinado: 'sim',
-            porte: 'medio',
+            porte: 'pequeno',
             castrado: 'sim',
-            pelagem: 'curta'
+            pelagem: 'curta',
+            raca: 'Salsicha'
         },
         {
             id: 6,
@@ -77,64 +82,76 @@ export const usePetStore = defineStore('petStore', () => {
             foto: '/pets_adocao_images/pet6.jpg',
             especie: 'gato',
             vacinado: 'nao',
-            porte: 'medio',
+            porte: 'pequeno',
             castrado: 'sim',
-            pelagem: 'media'
+            pelagem: 'media',
+            raca: 'Não possui'
+
         },
         {
             id: 7,
             nome: 'Charlie',
             genero: 'macho',
             foto: '/pets_adocao_images/pet7.jpg',
-            especie: 'cachorro',
+            especie: 'passaro',
             vacinado: 'parcialmente',
-            porte: 'grande',
+            porte: 'pequeno',
             castrado: 'nao',
-            pelagem: 'longa'
+            pelagem: 'longa',
+            raca: 'Não possui'
+
         },
         {
             id: 8,
-            nome: 'Lucy',
+            nome: 'Lily',
             genero: 'femea',
-            foto: '/pets_adocao_images/pet8.jpg',
-            especie: 'gato',
+            foto: '/pets_adocao_images/pet17.jpg',
+            especie: 'passaro',
             vacinado: 'sim',
             porte: 'pequeno',
-            castrado: 'nao',
-            pelagem: 'curta'
+            castrado: 'sim',
+            pelagem: 'curta',
+            raca: 'Não possui'
+            
         },
         {
             id: 9,
             nome: 'Daisy',
             genero: 'femea',
             foto: '/pets_adocao_images/pet9.jpg',
-            especie: 'cachorro',
+            especie: 'passaro',
             vacinado: 'sim',
             porte: 'pequeno',
             castrado: 'sim',
-            pelagem: 'curta'
+            pelagem: 'curta',
+            raca: 'Não possui'
+
         },
         {
             id: 10,
-            nome: 'Rocky',
-            genero: 'macho',
-            foto: '/pets_adocao_images/pet10.jpg',
-            especie: 'gato',
-            vacinado: 'nao',
-            porte: 'medio',
+            nome: 'Misty',
+            genero: 'femea',
+            foto: '/pets_adocao_images/pet22.jpg',
+            especie: 'outros',
+            vacinado: 'sim',
+            porte: 'pequeno',
             castrado: 'nao',
-            pelagem: 'longa'
+            pelagem: 'curta',
+            raca: 'Não possui'
+            
         },
         {
             id: 11,
             nome: 'Zoe',
             genero: 'femea',
             foto: '/pets_adocao_images/pet11.jpg',
-            especie: 'cachorro',
+            especie: 'gato',
             vacinado: 'parcialmente',
-            porte: 'grande',
+            porte: 'pequeno',
             castrado: 'sim',
-            pelagem: 'media'
+            pelagem: 'media',
+            raca: 'Não possui'
+
         },
         {
             id: 12,
@@ -143,7 +160,7 @@ export const usePetStore = defineStore('petStore', () => {
             foto: '/pets_adocao_images/pet12.jpg',
             especie: 'gato',
             vacinado: 'sim',
-            porte: 'pequeno',
+            porte: 'medio',
             castrado: 'nao',
             pelagem: 'curta'
         },
@@ -152,133 +169,223 @@ export const usePetStore = defineStore('petStore', () => {
             nome: 'Coco',
             genero: 'femea',
             foto: '/pets_adocao_images/pet13.jpg',
-            especie: 'cachorro',
+            especie: 'gato',
             vacinado: 'parcialmente',
-            porte: 'grande',
+            porte: 'medio',
             castrado: 'sim',
-            pelagem: 'media'
+            pelagem: 'media',
+            raca: 'Não possui'
+            
         },
         {
             id: 14,
             nome: 'Tiger',
             genero: 'macho',
             foto: '/pets_adocao_images/pet14.jpg',
-            especie: 'gato',
+            especie: 'cachorro',
             vacinado: 'sim',
-            porte: 'pequeno',
+            porte: 'medio',
             castrado: 'nao',
-            pelagem: 'curta'
+            pelagem: 'curta',
+            raca: 'Vira-lata'
+
         },
         {
             id: 15,
             nome: 'Molly',
             genero: 'femea',
             foto: '/pets_adocao_images/pet15.jpg',
-            especie: 'cachorro',
+            especie: 'passaro',
             vacinado: 'nao',
-            porte: 'medio',
+            porte: 'pequeno',
             castrado: 'sim',
-            pelagem: 'longa'
+            pelagem: 'longa',
+            raca: 'Não possui'
+
         },
         {
             id: 16,
             nome: 'Bruno',
             genero: 'macho',
             foto: '/pets_adocao_images/pet16.jpg',
-            especie: 'gato',
+            especie: 'passaro',
             vacinado: 'parcialmente',
-            porte: 'grande',
+            porte: 'pequeno',
             castrado: 'nao',
-            pelagem: 'media'
+            pelagem: 'media',
+            raca: 'Não possui'
+
         },
         {
             id: 17,
-            nome: 'Lily',
+            nome: 'Lucy',
             genero: 'femea',
-            foto: '/pets_adocao_images/pet17.jpg',
+            foto: '/pets_adocao_images/pet8.jpg',
             especie: 'cachorro',
             vacinado: 'sim',
-            porte: 'pequeno',
-            castrado: 'sim',
-            pelagem: 'curta'
+            porte: 'medio',
+            castrado: 'nao',
+            pelagem: 'curta',
+            raca: 'Não possui'
+
         },
         {
             id: 18,
             nome: 'Oscar',
             genero: 'macho',
             foto: '/pets_adocao_images/pet18.jpg',
-            especie: 'gato',
+            especie: 'passaro',
             vacinado: 'nao',
-            porte: 'medio',
+            porte: 'pequeno',
             castrado: 'nao',
-            pelagem: 'longa'
+            pelagem: 'longa',
+            raca: 'Vira-lata'
+
         },
         {
             id: 19,
-            nome: 'Simba',
-            genero: 'macho',
-            foto: '/pets_adocao_images/pet19.jpg',
+           nome: 'Mia',
+            genero: 'femea',
+            foto: '/pets_adocao_images/pet2.jpg',
             especie: 'cachorro',
-            vacinado: 'sim',
+            vacinado: 'nao',
             porte: 'medio',
-            castrado: 'sim',
-            pelagem: 'curta'
+            castrado: 'nao',
+            pelagem: 'longa',
+            raca: 'Não possui'
+
         },
         {
             id: 20,
             nome: 'Nala',
             genero: 'femea',
             foto: '/pets_adocao_images/pet20.jpg',
-            especie: 'gato',
+            especie: 'cachorro',
             vacinado: 'nao',
-            porte: 'pequeno',
+            porte: 'medio',
             castrado: 'nao',
-            pelagem: 'longa'
+            pelagem: 'longa',
+            raca: 'Fiapo de manga'
+
         },
         {
             id: 21,
             nome: 'Leo',
             genero: 'macho',
             foto: '/pets_adocao_images/pet21.jpg',
-            especie: 'cachorro',
+            especie: 'outros',
             vacinado: 'parcialmente',
-            porte: 'grande',
+            porte: 'medio',
             castrado: 'sim',
-            pelagem: 'media'
+            pelagem: 'media',
+            raca: 'Não possui'
+
         },
         {
             id: 22,
-            nome: 'Misty',
-            genero: 'femea',
-            foto: '/pets_adocao_images/pet22.jpg',
-            especie: 'gato',
-            vacinado: 'sim',
-            porte: 'pequeno',
+            nome: 'Rocky',
+            genero: 'macho',
+            foto: '/pets_adocao_images/pet10.jpg',
+            especie: 'cachorro',
+            vacinado: 'nao',
+            porte: 'medio',
             castrado: 'nao',
-            pelagem: 'curta'
+            pelagem: 'longa',
+            raca: 'Vira-lata'
+
         },
         {
             id: 23,
             nome: 'Rex Jr.',
             genero: 'macho',
             foto: '/pets_adocao_images/pet23.jpg',
-            especie: 'cachorro',
+            especie: 'outros',
             vacinado: 'sim',
-            porte: 'medio',
+            porte: 'pequeno',
             castrado: 'sim',
-            pelagem: 'curta'
+            pelagem: 'curta',
+            raca: 'Não possui'
+
         },
         {
             id: 24,
-            nome: 'Luna Jr.',
+            nome: 'Luna',
             genero: 'femea',
             foto: '/pets_adocao_images/pet24.jpg',
             especie: 'gato',
             vacinado: 'nao',
             porte: 'medio',
             castrado: 'sim',
-            pelagem: 'media'
+            pelagem: 'media',
+            raca: 'Não possui'
+
         },
+        {
+            id: 25,
+            nome: 'Rocky Jr.',
+            genero: 'macho',
+            foto: '/pets_adocao_images/pet25.jpg',
+            especie: 'cachorro',
+            vacinado: 'sim',
+            porte: 'medio',
+            castrado: 'sim',
+            pelagem: 'curta'
+    },
+    {
+            id: 26,
+            nome: 'Maggie',
+            genero: 'femea',
+            foto: '/pets_adocao_images/pet26.jpg',
+            especie: 'gato',
+            vacinado: 'sim',
+            porte: 'pequeno',
+            castrado: 'nao',
+            pelagem: 'longa'
+    },
+    {
+            id: 27,
+            nome: 'Jack',
+            genero: 'macho',
+            foto: '/pets_adocao_images/pet27.jpg',
+            especie: 'gato',
+            vacinado: 'nao',
+            porte: 'medio',
+            castrado: 'nao',
+            pelagem: 'curta'
+    },
+    {
+            id: 28,
+            nome: 'Dolly',
+            genero: 'femea',
+            foto: '/pets_adocao_images/pet28.jpg',
+            especie: 'gato',
+            vacinado: 'sim',
+            porte: 'pequeno',
+            castrado: 'sim',
+            pelagem: 'media'
+    },
+    {
+            id: 29,
+            nome: 'Zeus',
+            genero: 'macho',
+            foto: '/pets_adocao_images/pet29.jpg',
+            especie: 'cachorro',
+            vacinado: 'parcialmente',
+            porte: 'pequeno',
+            castrado: 'sim',
+            pelagem: 'curta'
+    },
+    {
+            id: 30,
+            nome: 'Lola',
+            genero: 'femea',
+            foto: '/pets_adocao_images/pet30.jpg',
+            especie: 'gato',
+            vacinado: 'sim',
+            porte: 'pequeno',
+            castrado: 'nao',
+            pelagem: 'media'
+    },
     ]);
 
     const filterStore = useFilterStore();
