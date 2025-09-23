237f0314fd4587c740c2a7a80980be7be9ee6225
AndreRicardoSc <andrenarutto7@gmail.com>
2025-09-23 16:37:26 -0300

    FEAT: Change urlStore #103

--- src/stores/RequestsUrls.js
+++ src/stores/RequestsUrls.js
@@ -2,16 +2,18 @@ import { defineStore } from "pinia";
 
 export const useRequestUrlStore = defineStore('requestUrlStore', () => {
     const urls = {
-        //endpoints para puxar as informações que aparecerão no site
-        organizacoes: '/organizacoes/',
-        perdidos: '/perdidos/',
-        pets: '/pets/',
-        portes: '/portes/',
-        racas: '/racas/',
-        //registro e autenticação de usuários
-        register: '/auth/register/',
-        refresh: '/auth/refresh/',
-        token: '/auth/token/',
+        //Listagem
+        organizationList: 'organizations/',
+        userList: 'users/',
+        adocao: 'adocao/',
+        perdidos: 'perdidos/',
+
+        //Usuários
+        register: 'register/',
+        login: 'login/',
+        refresh: 'refresh/',
+        delete: 'delete-user/',
+        update: 'update/',
     }
 
     return (
