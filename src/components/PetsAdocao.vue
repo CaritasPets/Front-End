34eeef898734f065e66c3b57530cce2a3e01006a
AmandaRibeiroLuz <amanda.ribeiro.da.luz.201@gmail.com>
2025-09-23 18:06:14 -0300

    FEAT: Correcting responsiveness of cards. #105

--- src/components/PetsAdocao.vue
+++ src/components/PetsAdocao.vue
@@ -13,18 +13,18 @@ function capitalize(str) {
 
 <template>
   <div
-    class="bg-[#EEEBD1] flex flex-col overflow-hidden rounded-3xl items-center cursor-pointer mb-5 mx-2 w-[160px] h-[220px] transition-all pb-6 ease-out hover:shadow-xl sm:mx-6 sm:mb-14 sm:w-[200px]  sm:h-[230px] md:w-[220px] md:h-[260px] lg:w-[210px] lg:h-[285px]"
+    class="bg-[#EEEBD1] flex flex-col overflow-hidden rounded-3xl items-center cursor-pointer mb-5 mx-2 w-[160px] h-[260px] transition-all pb-6 ease-out hover:shadow-xl sm:mx-6 sm:mb-14 sm:w-[200px] sm:h-[230px] md:w-[220px] md:h-[260px] lg:w-[210px] lg:h-[290px]"
     @click="goToProfile">
-    <img class="w-[215px] h-[215px] object-cover" :src="props.foto" :alt="`${props.nome}-image`">
+    <img class="w-[215px] h-[215px] max-w-[215px] max-h-[180px] md:max-h-[215px] object-cover" :src="props.foto" :alt="`${props.nome}-image`">
     <div class="flex">
-      <div class="flex flex-col items-left ">
+      <div class="flex flex-col pl-10 md:pl-0 md:items-left">
         <p
-          class="my-1 text-[1rem]  text-black text-left truncate overflow-hidden w-35 sm:text-[1.3rem] md:w-40 font-[Sen]">
+          class="my-1 text-[1rem] text-black text-center md:text-left truncate overflow-hidden pt-2 w-35 sm:text-[1.3rem] md:w-40 font-[Sen]">
           {{ props.nome }}</p>
-        <p class="my-1 text-[0.8rem]  text-black -mt-2 mb-5.5 text-left sm:text-[1rem] font-[Sen]">{{ capitalize(props.genero) }}
+        <p class="my-1 text-[0.8rem] text-black -mt-2 mb-5.5 text-center md:text-left sm:text-[1rem] font-[Sen]">{{ capitalize(props.genero) }}
         </p>
       </div>
-      <span class="mdi mdi-heart-outline text-black text-2xl mt-4"></span>
+      <span class="mdi mdi-heart-outline text-black text-2xl mt-4 pr-25 md:pr-0"></span>
     </div>
   </div>
 </template>
