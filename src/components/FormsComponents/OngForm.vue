d1dafed91923092032537629c5d80f3f3d1f097a
Johann5731 <johannmborges27@gmail.com>
2025-09-24 22:27:42 -0300

    FEAT: Small improvements #107

--- src/components/FormsComponents/OngForm.vue
+++ src/components/FormsComponents/OngForm.vue
@@ -4,7 +4,7 @@ import BaseInput from '../InputComponents/BaseInput.vue'
 
 </script>
 <template>
-  <form class="flex flex-col items-center py-20 gap-10 bg-amber-200 sm:gap-16 lg:gap-20">
+  <form class="flex flex-col items-center py-20 gap-10 bg-[#F7F5E0] sm:gap-16 lg:gap-20">
     <div class="sm:flex md:flex lg:flex justify-around gap-10 w-full">
         <ul class="flex flex-col items-center w-full lg:w-1/3 gap-6">
           <li class="flex flex-col w-[100%]">
@@ -41,21 +41,21 @@ import BaseInput from '../InputComponents/BaseInput.vue'
     <div class="mt-14 mr-180">
       <h2 class="text-xl font-[Sen] text-[#1E0B00]">* Indica campo obrigatório.</h2>
     </div>
-    <div class="flex justify-center gap-4 sm:gap-30">
-      <button
-        class="text-xl rounded-xl py-2 pl-4 pr-6 bg-[#FDA202] cursor-pointer border-2 border-transparent transition-all duration-500 hover:bg-transparent hover:border-[#FDA202] hover:text-[#FDA202]"
-        type="reset"
-      >
-        <span class="mr-2 mdi mdi-delete-outline font-[Sen]"></span>
-        Limpar
-      </button>
-      <button
-        class="text-xl rounded-xl py-2 pl-5 pr-6 bg-[#FF7700] cursor-pointer border-2 border-transparent transition-all duration-500 hover:bg-transparent hover:border-[#FF7700] hover:text-[#FF7700]"
-        type="submit"
-      >
-        <span class="mr-2 mdi mdi-paw font-[Sen]"></span>
-        Cadastrar ONG
-      </button>
-    </div>
+    <div>
+        <ul>
+          <li class="w-full flex flex-row justify-center gap-4 sm:gap-10 ">
+            <button
+              class="text-xl rounded-xl py-2 px-6 bg-[#FFBC46] cursor-pointer border-2 border-transparent transition-all duration-500 hover:bg-transparent hover:border-[#FFBC46] hover:text-[#FFBC46] font-[Sen]"
+              type="reset">
+              Limpar
+            </button>
+            <button
+              class="text-xl rounded-xl py-2 px-6 bg-[#FF953C] cursor-pointer border-2 border-transparent transition-all duration-500 hover:bg-transparent hover:border-[#FF953C] hover:text-[#FF953C] font-[Sen]"
+              type="submit">
+              Cadastrar ONG
+            </button>
+          </li>
+        </ul>
+      </div>
   </form>
 </template>
--- src/components/FormsComponents/PerdidoForm.vue
+++ src/components/FormsComponents/PerdidoForm.vue
@@ -23,7 +23,7 @@ const getValues = () => {
 }
 </script>
 <template>
-  <form @submit.prevent="handleSubmit" class="flex flex-col items-center py-20 gap-10 bg-amber-200 sm:gap-16 lg:gap-20">
+  <form @submit.prevent="handleSubmit" class="flex flex-col items-center py-20 gap-10 bg-[#F7F5E0] sm:gap-16 lg:gap-20">
     <div class="sm:flex md:flex lg:flex justify-around gap-10 w-full">
         <ul class="flex flex-col items-center w-full lg:w-1/3 gap-6">
           <li class="flex flex-col w-[100%]">
@@ -36,36 +36,36 @@ const getValues = () => {
             <BaseInput name="input3"/>
           </li>
         </ul>
-        <ul>
-          <li class="flex flex-col w-[100%]">
-            <PictureInput class="mb-5"/>
-          </li>
-          <li class="flex flex-col w-[100%]">
-            <ToggleComponent name="radio2" class="mb-5"/>
-          </li>
+        <ul class="flex flex-col items-center w-full lg:w-1/3 gap-6">
           <li class="flex flex-col w-[100%]">
             <ToggleComponent name="radio1"/>
+          </li>
+          <li class="flex flex-col w-[100%]">
+            <ToggleComponent name="radio2"/>
+          </li>
+          <li class="flex flex-col w-[100%]">
+            <PictureInput/>
           </li>
         </ul>
     </div>
     <div class="mt-14 mr-180">
       <h2 class="text-xl font-[Sen] text-[#1E0B00]">* Indica campo obrigatório.</h2>
     </div>
-    <div class="flex justify-center gap-30">
-      <button
-        class="mt-6 text-xl text-[#FFF493] rounded-xl py-2 px-6 bg-[#03497B] cursor-pointer border-2 border-transparent transition-all duration-500 hover:bg-transparent hover:border-[#03497B] hover:text-[#03497B] font-[Sen]"
-        type="reset"
-      >
-        <span class="mr-2 mdi mdi-delete-outline"></span>
-        Limpar
-      </button>
-      <button
-        class="mt-6 text-xl text-[#FFF493] rounded-xl py-2 px-6 bg-[#104C00] cursor-pointer border-2 border-transparent transition-all duration-500 hover:bg-transparent hover:border-[#104C00] hover:text-[#104C00] font-[Sen]"
-        type="submit"
-      >
-        <span class="mdi mdi-paw"></span>
-        Cadastrar Pet Perdido
-      </button>
-    </div>
+    <div>
+        <ul>
+          <li class="w-full flex flex-row justify-center gap-4 sm:gap-10 ">
+            <button
+              class="text-xl rounded-xl py-2 px-6 bg-[#FFBC46] cursor-pointer border-2 border-transparent transition-all duration-500 hover:bg-transparent hover:border-[#FFBC46] hover:text-[#FFBC46] font-[Sen]"
+              type="reset">
+              Limpar
+            </button>
+            <button
+              class="text-xl rounded-xl py-2 px-6 bg-[#FF953C] cursor-pointer border-2 border-transparent transition-all duration-500 hover:bg-transparent hover:border-[#FF953C] hover:text-[#FF953C] font-[Sen]"
+              type="submit">
+              Cadastrar Pet
+            </button>
+          </li>
+        </ul>
+      </div>
   </form>
 </template>
