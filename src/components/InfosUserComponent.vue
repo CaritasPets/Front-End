1d98d73024fa622aa0f8d0611401cf7e40c539c1
AndreRicardoSc <andrenarutto7@gmail.com>
2025-10-04 11:57:30 -0300

    FIX: Little alteration in the InfosUserComponent

--- src/components/InfosUserComponent.vue
+++ src/components/InfosUserComponent.vue
@@ -33,14 +33,14 @@ onMounted(() => {
         </li>
         <li v-else class="flex flex-col items-center justify-center bg-[#EEEBD1] rounded-2xl p-4 w-[100px] sm:w-[180px] md:w-[200px]"><img src="/petInformationIcons/id-icon.svg" alt="Ícone CPF" class="w-12 h-12 mb-2"><strong>CNPJ:</strong> {{ props?.user?.organization_profile.cnpj }}
         </li>
-        <li v-if="props?.user?.common_profile" class="flex flex-col items-center justify-center bg-[#EEEBD1] rounded-2xl p-4 w-[100px] sm:w-[180px] md:w-[200px]"><img src="/petInformationIcons/date-icon.svg" alt="Ícone Data" class="w-12 h-12 mb-2"><strong>Data de Nascimento:</strong> {{ props?.user?.common_profile.data_nascimento }}
+        <li v-if="props?.user?.common_profile" class="flex flex-col items-center justify-center bg-[#EEEBD1] rounded-2xl p-4 w-[100px] sm:w-[180px] md:w-[200px]"><img src="/petInformationIcons/date-icon.svg" alt="Ícone Data" class="w-12 h-12 mb-2"><strong class="sm:text-center">Data de Nascimento:</strong> {{ props?.user?.common_profile.data_nascimento }}
         </li>
-        <li class="flex flex-col items-center justify-center bg-[#EEEBD1] rounded-2xl p-4 w-[100px] sm:w-[180px] md:w-[200px]"> <img src="/petInformationIcons/mail-icon.svg" alt="Ícone Email" class="w-12 h-12 mb-2"><strong>Email:</strong>
+        <li class="flex flex-col items-center justify-center bg-[#EEEBD1] rounded-2xl p-4 w-[100px] sm:w-[180px] md:w-[250px]"> <img src="/petInformationIcons/mail-icon.svg" alt="Ícone Email" class="w-12 h-12 mb-2"><strong>Email:</strong>
           {{ props?.user?.email }}
         </li>
       </ul>
     </div>
-    <div class="text-center flex flex-wrap mb-10 gap-5 justify-center bg-[#F7F5E0] pt-10 lg:ml-65 sm:max-w-[1000px] md:gap-10">
+    <div class="flex flex-wrap mb-10 gap-5 justify-center bg-[#F7F5E0] pt-10 lg:ml-65 sm:max-w-[1000px] md:gap-10">
       <button @click="$emit('logout')" class="bg-[#98CB2A] mt-10 w-[50%] text-lg pt-2 pb-2 rounded-2xl font-[Sen] cursor-pointer border-2 border-transparent transition-all duration-500 hover:bg-transparent hover:border-[#98CB2A] hover:text-[#2c2e27] sm:w-[20%]"><span class="mdi mdi-arrow-left-bold-outline"></span> Sair
       </button>
       <button class="bg-[#40A9F4] mt-10 w-[70%] text-lg pt-2 pb-2 rounded-2xl font-[Sen] cursor-pointer border-2 border-transparent transition-all duration-500 hover:bg-transparent hover:border-[#40A9F4] hover:text-[#40A9F4] sm:w-[30%]"><span class="mdi mdi-pencil-outline"></span> Alterar informações
